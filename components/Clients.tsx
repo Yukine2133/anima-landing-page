d5a77dcc4419957264e16bdd26d8b5d0b66b70c6
Yukine213 <kondratenkov1109@gmail.com>
2024-03-13 16:00:25 +0200

    forgot to add border

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Clients = () => {
   return (
-    <div className="grid mt-[3.785rem]  grid-cols-3 md:grid-cols-6  place-items-center mx-auto md:place-content-evenly gap-[3.25rem] md:gap-14  lg::gap-[72px] px-4 xl:px-14">
+    <div className="grid mt-[3.785rem] lg:mt-[6.25rem] xl:mt-[7.525rem]  grid-cols-3 md:grid-cols-6  place-items-center mx-auto md:place-content-evenly gap-[3.25rem] md:gap-14  lg::gap-[72px] px-4 xl:px-14 border-y py-10 w-[90%] xl:w-3/4">
       {clients.map((item) => (
         <Image
           src={item.image}
